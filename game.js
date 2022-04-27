1e3ea95d3598ec5ba05c46159bb56f53e3b42876
MosheHadadG <Moshehadad500@gmail.com>
2022-04-27 21:51:03 +0300

    Add Snake Game

---
+++ game.js
@@ -0,0 +1,45 @@
+import { update as updateSnake, draw as drawSnake, snakeSpeed, getSnakeHead, snakeIntersection} from './snake.js'
+import { update as updateFood, draw as drawFood } from './food.js'
+import { outSideGrid } from './grid.js'
+let lastRenderTime = 0;
+let gameOver = false;
+const gameBoard = document.querySelector('#game-board')
+
+function main(currentTime) {
+  if (gameOver) {
+    if (confirm('You lost. Press ok to new game.')) {
+      window.location.reload();
+    }
+    return
+    
+  }
+  window.requestAnimationFrame(main)
+  const secondSinceLastRender = (currentTime - lastRenderTime) / 1000
+  if (secondSinceLastRender < 1 / snakeSpeed) return;
+  
+  lastRenderTime = currentTime;
+  
+  update()
+  draw()
+  
+}
+
+window.requestAnimationFrame(main)
+
+function update() {
+  updateSnake()
+  updateFood()
+  checkDeath()
+}
+
+
+function draw() {
+  gameBoard.innerHTML = ""
+  drawSnake(gameBoard)
+  drawFood(gameBoard)
+}
+
+function checkDeath() {
+  gameOver = outSideGrid(getSnakeHead()) || snakeIntersection()
+}
+
